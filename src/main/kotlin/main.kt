ea604658d9d38db252581681a6b4d04eb80ddce7
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-04-14 16:01:45 +0300

    inputoutput

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    wow()
+    love()
+    classy()
+}
+fun wow(){
+    print("Enter a text: ")
+    val life = readLine()
+    println("You have entered $life")
+}
+fun love(){
+    print("Enter a text: ")
+    val food = readLine()
+    println("This is the statement Ashly said - $food")
+}
+fun classy(){
+    print("Enter a text: ")
+    val lace = readLine()
+    println("I hope we will improve our results as $lace")
+}
